a02b6846915c733405c1a3ca8b9e80ac5f6a37e6
ekanath <74334316+Ekanath-K@users.noreply.github.com>
2024-10-06 13:01:19 -0700

    Update transcript.py

--- transcript.py
+++ transcript.py
@@ -22,3 +22,61 @@ if transcript.status == aai.TranscriptStatus.error:
     print(transcript.error)
 else:
     print(transcript.text)
+
+# Start by making sure the `assemblyai` package is installed.
+# If not, you can install it by running the following command:
+# pip install -U assemblyai
+#
+# Note: Some macOS users may need to use `pip3` instead of `pip`.
+
+import assemblyai as aai
+
+# Replace with your API key
+aai.settings.api_key = "a10344be1c7e4b55b7c5bdb73b27318e"
+
+# URL of the file to transcribe
+FILE_URL = "https://assembly.ai/wildfires.mp3"
+
+# You can also transcribe a local file by passing in a file path
+# FILE_URL = './path/to/file.mp3'
+
+config = aai.TranscriptionConfig(speaker_labels=True)
+
+transcriber = aai.Transcriber()
+transcript = transcriber.transcribe(
+  FILE_URL,
+  config=config
+)
+
+for utterance in transcript.utterances:
+  print(f"Speaker {utterance.speaker}: {utterance.text}")
+
+
+# Start by making sure the `assemblyai` package is installed.
+# If not, you can install it by running the following command:
+# pip install -U assemblyai
+#
+# Note: Some macOS users may need to use `pip3` instead of `pip`.
+
+import assemblyai as aai
+
+# Replace with your API key
+aai.settings.api_key = "a10344be1c7e4b55b7c5bdb73b27318e"
+
+# URL of the file to transcribe
+FILE_URL = "https://assembly.ai/wildfires.mp3"
+
+# You can also transcribe a local file by passing in a file path
+# FILE_URL = './path/to/file.mp3'
+
+config = aai.TranscriptionConfig(auto_highlights=True)
+
+transcriber = aai.Transcriber()
+transcript = transcriber.transcribe(
+  FILE_URL,
+  config=config
+)
+
+for result in transcript.auto_highlights.results:
+  print(f"Highlight: {result.text}, Count: {result.count}, Rank: {result.rank}")
+
